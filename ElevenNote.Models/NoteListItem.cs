1343baa98160cc95edb8c85811d7b217b2ad48b0
Erika <erikapeltz0@gmail.com>
2021-03-27 16:22:31 -0400

    Starred

--- ElevenNote.Models/NoteListItem.cs
+++ ElevenNote.Models/NoteListItem.cs
@@ -13,5 +13,8 @@ namespace ElevenNote.Models
         public string Title { get; set; }
         [Display(Name = "Created")]
         public DateTimeOffset CreatedUtc { get; set; }
+
+        [UIHint("Starred")]
+        public bool IsStarred { get; set; }
     }
 }
